0e933edca45e2b47368b5e05196569ca1576f509
Wellici Araujo <wja1@ifal.edu.br>
2019-12-28 14:41:52 -0300

    structure basic

---
+++ src/routes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Main from '../pages/Main';
+
+export default function Routes() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Main} exact />
+      </Switch>
+    </Router>
+  );
+}
